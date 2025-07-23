f5b41d2ab8cf0519342159b6e04f63f3eab352ac
davidcm146 <quachhuycm@gmail.com>
2025-07-23 15:14:12 +0700

    complete task E

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: %i[ show edit update destroy ]
+  rescue_from ActiveRecord::RecordNotFound, with: :cart_not_found
 
   # GET /carts or /carts.json
   def index
@@ -8,6 +9,7 @@ class CartsController < ApplicationController
 
   # GET /carts/1 or /carts/1.json
   def show
+    redirect_to store_index_path, notice: "Access denied." unless @cart.id == session[:cart_id]
   end
 
   # GET /carts/new
@@ -49,10 +51,11 @@ class CartsController < ApplicationController
 
   # DELETE /carts/1 or /carts/1.json
   def destroy
-    @cart.destroy!
+    @cart.destroy! if @cart.id === session[:cart_id]
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to carts_path, status: :see_other, notice: "Cart was successfully destroyed." }
+      format.html { redirect_to store_index_path, status: :see_other, notice: "Cart was empty." }
       format.json { head :no_content }
     end
   end
@@ -61,6 +64,11 @@ class CartsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
       @cart = Cart.find(params[:id])
+    end
+
+    def cart_not_found
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_index_path, notice: "Invalid cart"
     end
 
     # Only allow a list of trusted parameters through.
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,7 +24,7 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
@@ -55,7 +55,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy!
 
     respond_to do |format|
-      format.html { redirect_to line_items_path, status: :see_other, notice: "Line item was successfully destroyed." }
+      format.html { redirect_to store_index_path, status: :see_other, notice: "Line item was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -68,6 +68,6 @@ class LineItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
